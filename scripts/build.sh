f15bc02e3c8965620d79268f01710336f472ee99
Qeuroal <12997837+qeuroal@user.noreply.gitee.com>
2024-11-02 18:56:35 +0800

    build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+#{{{> .dwm
+#
 if [ ! -d ~/.dwm ]; then
     mkdir -p ~/.dwm
 fi
 cp ./scripts/autostart.sh ~/.dwm/autostart.sh
 ln -sf `realpath ./scripts` ~/.dwm/
 ln -sf `realpath ./scripts/status_config` ~/.dwm/
+#<}}}
 
-# 打patch
+#{{{> .xinitrc
+#
+if [ ! -e ~/.xinitrc ] || [ `cat ~/.xinitrc | grep -c "exec dwm"` -eq 0 ]; then
+    echo "exec dwm" >> ~/.xinitrc
+fi
+#<}}}
+
+#{{{> patch
+#
 if [ -d "patch" ]; then
     ls patch | grep "diff" | xargs -I {} cat ./patch/{} | patch
 fi
@@ -15,6 +26,7 @@ fi
 if [ -d "patch/after" ]; then
     ls patch/after | grep "diff" | xargs -I {} cat ./patch/after/{} | patch
 fi
+#<}}}
 
 # make
 make
